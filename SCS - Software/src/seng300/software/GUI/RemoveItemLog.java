c32fac49df66c285feac7181921a678cf334cd8d
Joey-mi <69664864+Joey-mi@users.noreply.github.com>
2022-04-07 23:11:19 -0600

    Made it so prices of products were displayed as well as total

--- SCS - Software/src/seng300/software/GUI/RemoveItemLog.java
+++ SCS - Software/src/seng300/software/GUI/RemoveItemLog.java
@@ -6,6 +6,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -26,6 +27,7 @@ import org.lsmr.selfcheckout.products.Product;
 import java.awt.Insets;
 import javax.swing.JTextArea;
 import java.awt.Font;
+import javax.swing.JTextField;
 
 public class RemoveItemLog extends JFrame implements ActionListener{
 
@@ -35,12 +37,14 @@ public class RemoveItemLog extends JFrame implements ActionListener{
 	private JSplitPane splitPane;
 	private GridBagConstraints gbc_txtrPleaseSelectAll;
 	private GridBagConstraints gbc_btnNewButton;
+	private GridBagConstraints gbc_printTotalprice;
 	private Dimension max;
 	private JTextArea txtrPleaseSelectAll;
 	private JButton remove;
 	private JCheckBox [] productsInLog;
 	private Map<JCheckBox, Product> removable;
 	private ArrayList<Product> allProducts;
+	private JTextField printTotalPrice;
 
 	/**
 	 * Create the frame.
@@ -61,8 +65,8 @@ public class RemoveItemLog extends JFrame implements ActionListener{
 
 		JPanel removeOrReturn = new JPanel();
 		GridBagLayout layout = new GridBagLayout();
-		layout.rowWeights = new double[]{0.0, 1.0};
-		layout.columnWeights = new double[]{0.0};
+		layout.rowWeights = new double[]{0.0, 1.0, 0.0};
+		layout.columnWeights = new double[]{1.0};
 		removeOrReturn.setLayout(layout);
 		
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, itemLog, removeOrReturn);
@@ -76,7 +80,7 @@ public class RemoveItemLog extends JFrame implements ActionListener{
 				+ "\n" + "    like to remove," 
 				+ "\n" + "then press remove");
 		gbc_txtrPleaseSelectAll = new GridBagConstraints();
-		gbc_txtrPleaseSelectAll.insets = new Insets(0, 0, 5, 5);
+		gbc_txtrPleaseSelectAll.insets = new Insets(0, 0, 5, 0);
 		gbc_txtrPleaseSelectAll.anchor = GridBagConstraints.NORTH;
 		gbc_txtrPleaseSelectAll.gridx = 0;
 		gbc_txtrPleaseSelectAll.gridy = 1;
@@ -90,9 +94,8 @@ public class RemoveItemLog extends JFrame implements ActionListener{
 		remove = new JButton("Remove");
 		remove.setFont(new Font("Tahoma", Font.BOLD, 12));
 		gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 2;
+		gbc_btnNewButton.gridy = 3;
 		removeOrReturn.add(remove, gbc_btnNewButton);
 		
 		splitPane.setDividerLocation(250);
@@ -105,18 +108,37 @@ public class RemoveItemLog extends JFrame implements ActionListener{
 		Product currentProduct;
 		BarcodedProduct bProduct;
 		PLUCodedProduct pProduct;
+		
+		BigDecimal productPrice;
+		double price;
+		double total = 0;
 		for (int i = 0; i < allProducts.size(); i++) {
 			currentProduct = allProducts.get(i);
+			
+			productPrice = currentProduct.getPrice();
+			price = productPrice.doubleValue();
+			total += price;
 			if (currentProduct instanceof BarcodedProduct) {
 				bProduct = (BarcodedProduct) currentProduct;
-				productsInLog[i] = new JCheckBox(bProduct.getDescription());
+				productsInLog[i] = new JCheckBox(bProduct.getDescription() + "\t" + "$" + price);
 			} else {
 				pProduct = (PLUCodedProduct) currentProduct;
-				productsInLog[i] = new JCheckBox(pProduct.getDescription());
+				productsInLog[i] = new JCheckBox(pProduct.getDescription()+ "\t" + "$" + price);
 			}
 			removable.put(productsInLog[i], currentProduct);
 			scrollPanel.add(productsInLog[i]);
 		}
+		
+		printTotalPrice = new JTextField("Total:" + "\t$" + total);
+		printTotalPrice.setFont(new Font("Tahoma", Font.BOLD, 14));
+		gbc_printTotalprice = new GridBagConstraints();
+		gbc_printTotalprice.insets = new Insets(0, 0, 5, 0);
+		gbc_printTotalprice.fill = GridBagConstraints.HORIZONTAL;
+		gbc_printTotalprice.gridx = 0;
+		gbc_printTotalprice.gridy = 2;
+		printTotalPrice.setEditable(false);
+		removeOrReturn.add(printTotalPrice, gbc_printTotalprice);
+		printTotalPrice.setColumns(10);
 	}
 
 	@Override
@@ -129,6 +151,6 @@ public class RemoveItemLog extends JFrame implements ActionListener{
 				// selectItemToRemove method
 			}
 		}
+		setVisible(false);
 	}
-
 }
